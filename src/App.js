caf02587164ba8b33abe2ca96e600af7dd533ab2
dula089 <dulani.20231280@iit.ac.lk>
2024-06-17 13:58:58 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import ForgotPass from './Components/ForgotPass';
+import { Routes,Route} from 'react-router-dom';
+
 
 function App() {
-  return (
+
+  return ( 
     <div>
-     <ForgotPass></ForgotPass>
+      <Routes>
+         <Route path='/' element={<Login/>} />
+         <Route path='/signup' element={<Signup/>} />
+         <Route path='/forgot' element={<ForgotPass/>} />
+       </Routes>
+      {/* <Login></Login>
+      <Signup></Signup>
+     <ForgotPass></ForgotPass> */}
     </div>
   );
 }
