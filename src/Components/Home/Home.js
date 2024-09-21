bc9fbe305ddbf29f1eca3a57a62c39c338f192be
dula089 <dulani.20231280@iit.ac.lk>
2024-09-21 09:57:31 +0530

    Modify Home Page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,33 +1,77 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Post from '../Post/Post';
-import Nav from '../NavBar/Nav';
-import StoryBar from '../Story/StoryBar';
-import Create from '../Pages/Create/Create';
-import SideBar from '../SideBar/SideBar';
-import { setPosts } from '../redux/postsSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Post from "../Post/Post";
+import Nav from "../NavBar/Nav";
+import StoryBar from "../Story/StoryBar";
+import Create from "../Pages/Create/Create";
+import SideBar from "../SideBar/SideBar";
+import { setPosts } from "../redux/postsSlice";
+import SwitchAccount from "./SwitchAccount/SwitchAccount";
+import Footer from "../Footer/Footer";
+import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import { fetchAllPosts } from "../services/apiService"; 
 
 function Home() {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
+  const [switchAccount, setSwitchAccount] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
-    fetch('http://localhost:8080/post/getPosts')
-        .then((response) => response.json())
-        .then((data) => {
-            dispatch(setPosts(data));
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}, [dispatch]);
+    const getPosts = async () => {
+      try {
+        const data = await fetchAllPosts();
+        dispatch(setPosts(data));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getPosts();
+  }, [dispatch]);
+
+  const handleSwitchAccount = () => {
+    setScrollPosition(window.scrollY);
+    setSwitchAccount(true);
+  };
+
+  const handleCloseSwitchAccount = () => {
+    setSwitchAccount(false);
+  };
 
   return (
     <div>
       <Nav />
       <StoryBar />
-      <SideBar />
-      <Create />
+
+      <a
+        onClick={handleSwitchAccount}
+        style={{
+          position: "fixed",
+          top: "50px",
+          right: "100px",
+          zIndex: 10,
+          color: "blue",
+          cursor: "pointer",
+        }}
+      >
+        <ProfileHeader />
+        Switch
+      </a>
+
+      {switchAccount && (
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            zIndex: 15,
+            top: `${scrollPosition}px`,
+          }}
+        >
+          <SwitchAccount onClose={handleCloseSwitchAccount} />
+        </div>
+      )}
+
       {posts.map((post) => (
         <Post key={post.id} id={post.id} title={post.title} body={post.body} />
       ))}
